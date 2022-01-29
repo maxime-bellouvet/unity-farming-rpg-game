82f687c07951aed2dae433ac430a76aa1c8a8b08
Heisenberg <maxbellou@msn.com>
2022-01-29 20:05:38 +0100

    added player basics.

---
+++ Assets/Scripts/Enums/Enums.cs
@@ -0,0 +1,15 @@
+﻿
+public enum ToolEffect
+{
+    none,
+    watering
+}
+
+public enum Direction
+{
+    up,
+    down,
+    left,
+    right,
+    none
+}
---
+++ Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,31 @@
+﻿public delegate void MovementDelegate(float xInput, float yInput, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    ToolEffect toolEffect, bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+    bool idleRight, bool idleLeft, bool idleUp, bool idleDown);
+
+public static class EventHandler
+{
+    // Movement Event
+    public static event MovementDelegate MovementEvent;
+
+    // Movement event call for publishers
+    public static void CallMovementEvent(float xInput, float yInput, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+        ToolEffect toolEffect, bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+        bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+        bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+        bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+        bool idleRight, bool idleLeft, bool idleUp, bool idleDown)
+    {
+        if (MovementEvent != null)
+        {
+            MovementEvent(xInput, yInput, isWalking, isRunning, isIdle, isCarrying,
+            toolEffect, isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+            isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+            isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+            isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+            idleRight, idleLeft, idleUp, idleDown);
+        }
+    }
+}
